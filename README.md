5a3354bd43bdd93ee414fb3e30c0ab99b07fde30
Freewind <nowindlee@gmail.com>
2020-08-05 22:28:11 +0800

    typescript-react-use-async-memo-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript React Hello World Demo
-=================================
+TypeScript React "use-async-memo" Demo
+=====================================
+
+需要注意的是要给一个默认值，否则其值为undefined，但是在typing上反映不出来。
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,23 @@
 //import * as React from 'react'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useAsyncMemo} from "use-async-memo";
+
+async function fetchData(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('remote-hello')
+    }, 1000)
+  });
+}
 
 export default function Hello() {
+  const remoteData = useAsyncMemo(() => fetchData(), [], 'init-value')
+
+  useEffect(() => {
+    console.log("### remoteData", remoteData)
+  }, [remoteData])
+
   return <div>
-    <h1>Hello React</h1>
+    <h1>{remoteData}</h1>
   </div>
 };
